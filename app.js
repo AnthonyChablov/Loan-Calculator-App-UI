34278421b8e20da5b3ba2e8e2f1d3e73e6a069b7
Anthony Chablov <achablov@myseneca.ca>
2022-04-24 23:15:34 -0400

    Added Selections in app.js

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 
 console.log(1);
-alert(1);
+// alert(1);
+
+// Selecting elements
+
+let loanForm = document.querySelector('#loan-form');
+
+loanForm.addEventListener(('click'), ((e)=>{
+    console.log(e.type);
+    e.preventDefault();
+}))
